38762a8d5dedbc8beec2878f5ef8df7119553a6d
Aryan16032004 <aryan16062004@gmail.com>
2024-09-13 01:31:28 +0530

    url

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server:{
     proxy:{
-      '/api':'https://ssl-validator.onrender.com/'
+      '/api':'https://ssl-validator.onrender.com'
     }
   },
   plugins: [react()],
